8ab0a7620a7118b99e02a3a74fd770f52f1018b1
Swetha-stra <swetha.20240686@iit.ac.lk>
2025-11-13 22:40:27 +0530

    Add implementation for class methods

--- src/teammate/Team.java
+++ src/teammate/Team.java
@@ -1,4 +1,43 @@
 package teammate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private String teamName;
+    private List<Participant> members;
+
+    public Team(String teamName) {
+        this.teamName = teamName;
+        this.members = new ArrayList<>();
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public List<Participant> getMembers() {
+        return members;
+    }
+
+    public void addMember(Participant participant) {
+        members.add(participant);
+    }
+
+    public void removeMember(Participant participant) {
+        members.remove(participant);
+    }
+
+    public int getTeamSize() {
+        return members.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Team: " + teamName + "\nMembers:\n");
+        for (Participant p : members) {
+            sb.append(" - ").append(p.toString()).append("\n");
+        }
+        return sb.toString();
+    }
 }
