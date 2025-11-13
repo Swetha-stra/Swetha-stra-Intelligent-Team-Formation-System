9af8c602f9f43ec9122947db241ff8464fc3d3de
Swetha-stra <swetha.20240686@iit.ac.lk>
2025-11-13 22:47:40 +0530

    Add implementation for class methods

--- src/teammate/PersonalityClassifier.java
+++ src/teammate/PersonalityClassifier.java
@@ -1,4 +1,16 @@
 package teammate;
 
 public class PersonalityClassifier {
+
+    public static String classify(int score) {
+        if (score >= 90 && score <= 100) {
+            return "Leader";
+        } else if (score >= 70 && score <= 89) {
+            return "Balanced";
+        } else if (score >= 50 && score <= 69) {
+            return "Thinker";
+        } else {
+            return "Invalid Score";
+        }
+    }
 }
