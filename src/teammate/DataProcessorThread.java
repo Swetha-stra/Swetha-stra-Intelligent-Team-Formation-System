d3518e38eaeb09543a8e98080709d83d1781f36a
Swetha-stra <swetha.20240686@iit.ac.lk>
2025-11-13 22:53:37 +0530

    Add implementation for class methods

--- src/teammate/DataProcessorThread.java
+++ src/teammate/DataProcessorThread.java
@@ -1,4 +1,25 @@
 package teammate;
 
-public class DataProcessorThread {
+import java.util.List;
+
+public class DataProcessorThread extends Thread {
+    private TeamBuilder teamBuilder;
+    private List<Participant> participants;
+    private List<Team> formedTeams;
+
+    public DataProcessorThread(TeamBuilder teamBuilder, List<Participant> participants) {
+        this.teamBuilder = teamBuilder;
+        this.participants = participants;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread started: Processing data...");
+        formedTeams = teamBuilder.formTeams(participants);
+        System.out.println("Thread finished: Team formation complete!");
+    }
+
+    public List<Team> getFormedTeams() {
+        return formedTeams;
+    }
 }
